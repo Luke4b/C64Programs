25cd26c021ffff1e2e62adbb48777b7c64b3668e
Luke4b <46605428+Luke4b@users.noreply.github.com>
2022-09-11 10:04:21 +0100

    Create petscii_fill_assembly.asm

---
+++ petscii_fill_assembly.asm
@@ -0,0 +1,66 @@
+BasicUpstart2(main)
+
+* = $0810
+
+main:
+    jsr init
+    jsr loop
+
+init:
+    ldx #$00 //colour to use
+    ldy #$00 //screen position offset
+    lda #$00
+    sta $FA  //character to use
+    sta $FB  //screen page LSB
+    sta $FD  //colour page LSB
+    lda #$04
+    sta $FC  //screen page MSB
+    lda #$D8
+    sta $FE //colour page MSB
+
+loop:
+    jsr delay
+    txa
+    sta ($FD),y  // write colour to screen
+    jsr increment_colour
+    lda $FA      // load character
+    sta ($FB),y  // write to screen
+    iny
+    bne loop     // if y hasn't overflowed
+    inc $FC      // otherwise increment pages
+    inc $FE
+    lda #$08
+    cmp $FC
+    bne loop
+    lda #$04
+    sta $FC
+    lda #$D8
+    sta $FE
+    inc $FA
+    jmp loop
+
+increment_colour:
+    inx
+    cpx #$0F
+    bne return
+    ldx #$00
+    rts
+return: rts
+
+delay:
+    txa
+    pha
+    tya
+    pha
+    ldx #$FF
+    ldy #$02
+delay_loop:
+    dex
+    bne delay_loop
+    dey
+    bne delay_loop
+    pla
+    tay
+    pla
+    tax
+    rts
